a57926137d7cd8a048c4e52b46bb46f936a14e59
Gabriel Mascarenhas <devgabrielprog@gmail.com>
2025-02-12 21:01:45 -0300

    Implementação da dependência no pom do HATEOAS para finalização da API.

--- src/main/java/com/products_DatabaseConnective/startUp/controller/ProductControllers.java
+++ src/main/java/com/products_DatabaseConnective/startUp/controller/ProductControllers.java
@@ -8,9 +8,11 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class ProductControllers {
@@ -19,10 +21,45 @@ public class ProductControllers {
     ProductRepositories productRepositories;
 
     @PostMapping("/products")
-    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductDTO productDTO){
-        ProductModel productModel = new ProductModel();
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductDTO productDTO) {
+        var productModel = new ProductModel();
         BeanUtils.copyProperties(productDTO, productModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepositories.save(productModel));
     }
 
+    @GetMapping(value = "/products")
+    public ResponseEntity<List<ProductModel>> getAllProducts() {
+        return ResponseEntity.status(HttpStatus.OK).body(productRepositories.findAll());
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") UUID id) {
+        Optional<ProductModel> productO = productRepositories.findById(id);
+        if (productO.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(productO.get());
+    }
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") UUID id,
+                                                @RequestBody @Valid ProductDTO productDTO) {
+        Optional<ProductModel> productO = productRepositories.findById(id);
+        if (productO.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        var productModel = productO.get();
+        BeanUtils.copyProperties(productDTO, productModel);
+        return ResponseEntity.status(HttpStatus.OK).body(productRepositories.save(productModel));
+    }
+
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable(value = "id") UUID id) {
+        Optional<ProductModel> productO = productRepositories.findById(id);
+        if (productO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("product not found");
+        }
+        productRepositories.delete(productO.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully");
+    }
 }
--- src/main/java/com/products_DatabaseConnective/startUp/productRecordsDTO/ProductDTO.java
+++ src/main/java/com/products_DatabaseConnective/startUp/productRecordsDTO/ProductDTO.java
@@ -5,5 +5,5 @@ import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 
-public record ProductDTO(@NotBlank String name,@NotNull BigDecimal value) {
+public record ProductDTO(@NotBlank String nameProduct,@NotNull BigDecimal valueProduct) {
 }
