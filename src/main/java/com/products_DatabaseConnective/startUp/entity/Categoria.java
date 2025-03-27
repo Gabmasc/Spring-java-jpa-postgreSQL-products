e9d0c66306f10dcde9eb4884d0b601da5bd640ff
Gabriel Mascarenhas <devgabrielprog@gmail.com>
2025-03-27 08:49:50 -0300

    Nova entity aplicada: Produto.java

--- src/main/java/com/products_DatabaseConnective/startUp/entity/Categoria.java
+++ src/main/java/com/products_DatabaseConnective/startUp/entity/Categoria.java
@@ -15,6 +15,19 @@ public class Categoria {
     @Column(name = "categoria_nome")
     private String categoria_nome;
 
-    
+    public Integer getCategoria_id() {
+        return categoria_id;
+    }
 
+    public void setCategoria_id(Integer categoria_id) {
+        this.categoria_id = categoria_id;
+    }
+
+    public String getCategoria_nome() {
+        return categoria_nome;
+    }
+
+    public void setCategoria_nome(String categoria_nome) {
+        this.categoria_nome = categoria_nome;
+    }
 }
---
+++ src/main/java/com/products_DatabaseConnective/startUp/entity/Produto.java
@@ -0,0 +1,31 @@
+package com.products_DatabaseConnective.startUp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "produto_id")
+    private Integer produto_id;
+
+    @Column(name = "produto_nome")
+    private String produto_nome;
+
+    public Integer getProduto_id() {
+        return produto_id;
+    }
+
+    public void setProduto_id(Integer produto_id) {
+        this.produto_id = produto_id;
+    }
+
+    public String getProduto_nome() {
+        return produto_nome;
+    }
+
+    public void setProduto_nome(String produto_nome) {
+        this.produto_nome = produto_nome;
+    }
+}
